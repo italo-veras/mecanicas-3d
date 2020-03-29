197b0e00cf5b3c2b8faf476db10f509ddb3c86e4
italo-veras <italosousaveras@yahoo.com>
2020-03-29 00:10:31 -0300

    Pasta dos Assets

---
+++ Assets/_Scripts/TPS/MovimentThirdPerson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentThirdPerson : MovimentFirstPerson
+{
+   
+    protected override void Moviment()
+    {
+        base.Moviment();
+        LookInputs();
+    }
+    void LookMouse()
+    {
+
+    }
+    void LookInputs()
+    {
+        transform.LookAt(transform.position + new Vector3(inputMoviment.x, 0, inputMoviment.z));
+
+    }
+}
