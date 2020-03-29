01a6d6beab016b3d4b6aa9200d95d43fb4b72a44
italo-veras <italosousaveras@yahoo.com>
2020-03-28 23:56:28 -0300

    Configuraçoes para FPS

---
+++ Assets/_Scripts/FPS/CamFps.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFps : MonoBehaviour
+{
+
+    public Transform playerTransform;
+
+    public float mouseSensi;
+    public float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensi;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensi;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerTransform.Rotate(Vector3.up * mouseX);
+       // mouseMoviment();
+    }
+    void mouseMoviment()
+    {
+        
+
+    }
+}
---
+++ Assets/_Scripts/FPS/MovimentFirstPerson.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentFirstPerson : MonoBehaviour
+{
+
+    [SerializeField]
+    protected CharacterController characterController;
+    [SerializeField]
+    private Transform groundCheck;
+
+    protected Vector3 inputMoviment;
+    Vector3 gravityVector;
+
+    public float movementSpeed;
+    public float jumpSpeed;
+    float gravitySpeed = -29.43f;
+    public float groundCheckRadius;
+
+    public LayerMask groundMask;
+
+    public bool isGrounded;
+
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        groundCheck = GameObject.Find("Ground Check Player").GetComponent<Transform>();
+    }
+   
+    void Update()
+    {
+        Inputs();
+        
+    }
+    protected void FixedUpdate()
+    {
+        Moviment();
+        Gravity();
+    }
+    void Inputs()
+    {
+        inputMoviment = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")).normalized;
+       
+    }
+    protected virtual void Moviment()
+    {
+
+        characterController.Move(inputMoviment * movementSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gravityVector.y = Mathf.Sqrt(jumpSpeed * -2f * gravitySpeed);
+        }
+    } 
+    void Gravity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+
+        if (isGrounded == true && gravityVector.y < 0)
+        {
+            gravityVector.y = -1f;
+        }
+        else
+        {
+            gravityVector.y += gravitySpeed * Time.deltaTime;
+        }
+        characterController.Move(gravityVector * Time.deltaTime);
+    }
+    
+    
+}
